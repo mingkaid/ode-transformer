7aeea1beb1d5ab6c48754129e01bde8f6f07359c
qbiqing <bq2134@columbia.edu>
2019-04-07 13:19:05 -0400

    greedy decode, move to real data

--- train_copy_task.py
+++ train_copy_task.py
@@ -23,6 +23,9 @@ def data_gen(V, batch, nbatches):
         tgt = Variable(data, requires_grad=False)
         yield Batch(src, tgt, 0)
 
+"""
+Train and Evaluate with Greedy Decoding
+"""
 V = 11
 criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
 model = make_model(V, V, N=2)
@@ -37,3 +40,23 @@ for epoch in range(10):
     print(run_epoch(data_gen(V, 30, 5), model, 
                     SimpleLossCompute(model.generator, criterion, None)))
 
+def greedy_decode(model, src, src_mask, max_len, start_symbol):
+    memory = model.encode(src, src_mask)
+    ys = torch.ones(1, 1).fill_(start_symbol).type_as(src.data)
+    for i in range(max_len-1):
+        out = model.decode(memory, src_mask, 
+                           Variable(ys), 
+                           Variable(subsequent_mask(ys.size(1))
+                                    .type_as(src.data)))
+        prob = model.generator(out[:, -1])
+        _, next_word = torch.max(prob, dim = 1)
+        next_word = next_word.data[0]
+        ys = torch.cat([ys, 
+                        torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+    return ys
+
+
+model.eval()
+src = Variable(torch.LongTensor([[1,2,3,4,5,6,7,8,9,10]]) )
+src_mask = Variable(torch.ones(1, 1, 10) )
+print(greedy_decode(model, src, src_mask, max_len=10, start_symbol=1))
---
+++ train_dataset.py
@@ -0,0 +1,97 @@
+
+
+
+"""
+Train with actual dataset
+"""
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math, copy, time
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import seaborn
+from transformer import make_model
+from train_functions import get_std_opt, run_epoch,Batch, LabelSmoothing, NoamOpt, SimpleLossCompute
+seaborn.set_context(context="talk")
+
+"""
+Data Preprocessing
+"""
+from torchtext import data, datasets
+
+if True:
+    import spacy
+    spacy_de = spacy.load('de')
+    spacy_en = spacy.load('en')
+
+    def tokenize_de(text):
+        return [tok.text for tok in spacy_de.tokenizer(text)]
+
+    def tokenize_en(text):
+        return [tok.text for tok in spacy_en.tokenizer(text)]
+
+    BOS_WORD = '<s>'
+    EOS_WORD = '</s>'
+    BLANK_WORD = "<blank>"
+    SRC = data.Field(tokenize=tokenize_de, pad_token=BLANK_WORD)
+    TGT = data.Field(tokenize=tokenize_en, init_token = BOS_WORD, 
+                     eos_token = EOS_WORD, pad_token=BLANK_WORD)
+
+    MAX_LEN = 100
+    train, val, test = datasets.IWSLT.splits(
+        exts=('.de', '.en'), fields=(SRC, TGT), 
+        filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and 
+            len(vars(x)['trg']) <= MAX_LEN)
+    MIN_FREQ = 2
+    SRC.build_vocab(train.src, min_freq=MIN_FREQ)
+    TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
+
+
+"""
+Create batches
+"""
+class MyIterator(data.Iterator):
+    def create_batches(self):
+        if self.train:
+            def pool(d, random_shuffler):
+                for p in data.batch(d, self.batch_size * 100):
+                    p_batch = data.batch(
+                        sorted(p, key=self.sort_key),
+                        self.batch_size, self.batch_size_fn)
+                    for b in random_shuffler(list(p_batch)):
+                        yield b
+            self.batches = pool(self.data(), self.random_shuffler)
+            
+        else:
+            self.batches = []
+            for b in data.batch(self.data(), self.batch_size,
+                                          self.batch_size_fn):
+                self.batches.append(sorted(b, key=self.sort_key))
+
+def rebatch(pad_idx, batch):
+    "Fix order in torchtext to match ours"
+    src, trg = batch.src.transpose(0, 1), batch.trg.transpose(0, 1)
+    return Batch(src, trg, pad_idx)
+
+"""
+Train and evaluate
+"""
+
+model_opt = NoamOpt(model.src_embed[0].d_model, 1, 2000,
+        torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+for epoch in range(10):
+    model_par.train()
+    run_epoch((rebatch(pad_idx, b) for b in train_iter), 
+              model_par, 
+              SimpleLossCompute(model.generator, criterion, 
+                                  devices=devices, opt=model_opt))
+    model_par.eval()
+    loss = run_epoch((rebatch(pad_idx, b) for b in valid_iter), 
+                      model_par, 
+                      SimpleLossCompute(model.generator, criterion, 
+                      devices=devices, opt=None))
+    print(loss)
+
