5ed58a719d5f9443cc6d3c222ac2f2130c91a13d
qbiqing <bq2134@columbia.edu>
2019-04-07 20:22:04 -0400

    modified train function

--- train_functions.py
+++ train_functions.py
@@ -93,7 +93,7 @@ class LabelSmoothing(nn.Module):
         true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
         true_dist[:, self.padding_idx] = 0
         mask = torch.nonzero(target.data == self.padding_idx)
-        if mask.dim()>0:
+        if mask.sum() and len(mask)>0:
             true_dist.index_fill_(0, mask.squeeze(), 0.0)
         self.true_dist = true_dist
         return self.criterion(x, Variable(true_dist, requires_grad=False))
